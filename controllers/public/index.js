a65b6296f7e455bb35644c7d52644872c2c84850
Jerry Jones <Jerry.Jones@tn.gov>
2020-03-23 15:48:27 -0500

    changed mongo connection to TN mongo atlas

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const https = require('https');
 
 function index (req, res) {
     res.render('public/home.hbs',
@@ -11,6 +12,16 @@ function index (req, res) {
 
 function about(req,res) {
     // req.session.test_value = 'newTestValue';
+    let testUrl = 'https://state-of-tennessee-dev.apigee.net/mytn/apigee/ssmsService/message?numberTo=6147837538&messageTest=Hello';
+        https.get(testUrl, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        })
+    });
+
 
     let value = req.sessionID;
     let sessionValue = req.session.test_value;
