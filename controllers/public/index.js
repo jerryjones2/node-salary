2b506ecb37e5dbda82ba7efd1a96ece32c08595f
Jerry Jones <Jerry.Jones@tn.gov>
2020-03-23 16:20:20 -0500

    changed mongo connection to TN mongo atlas

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const https = require('https');
+const request = require('request');
 
 function index (req, res) {
     res.render('public/home.hbs',
@@ -10,17 +11,25 @@ function index (req, res) {
     )
 }
 
-function about(req,res) {
+function getIp(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) reject(error);
+            if (response.statusCode != 200) {
+                reject('Invalid status code <' + response.statusCode + '>');
+            }
+            resolve(body);
+        });
+    });
+}
+
+async function about(req,res) {
     // req.session.test_value = 'newTestValue';
     let testUrl = 'https://state-of-tennessee-dev.apigee.net/mytn/apigee/ssmsService/message?numberTo=6147837538&messageText=Hello';
-        https.get(testUrl, (resp) => {
-        let data = '';
+    let testUrl2 = 'https://api.ipify.org?format=json';
 
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        })
-    });
+    var data = await getIp(testUrl2);
+    
 
 
     let value = req.sessionID;
@@ -31,7 +40,7 @@ function about(req,res) {
     }
     res.render('public/about.hbs',
       {
-          pageTitle: 'About Page',
+          pageTitle: 'About Page (' + data + ')',
           message: 'Session test_value stored at: ' + sessionValue,
           layout: 'main'
       }
